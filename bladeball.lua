38dddbd5c60abea538c229b1e370a7251f597d30
MmbVNG <148338223+MmbVNG@users.noreply.github.com>
2023-10-26 19:11:32 +0700

    Update bladeball.lua

--- bladeball.lua
+++ bladeball.lua
@@ -16,6 +16,7 @@ local character = localPlayer.Character or localPlayer.CharacterAdded:Wait()
 local ballsFolder = workspace:WaitForChild("Balls")
 local parryButtonPress = replicatedStorage.Remotes.ParryButtonPress
 local abilityButtonPress = replicatedStorage.Remotes.AbilityButtonPress
+local sliderValue = 25
 local distanceVisualizer = nil
 local isRunning = false
 local notifyparried = false
@@ -61,33 +62,331 @@ else
     return
 end
  
-AutoParry:AddButton({
-	Name = "Auto Parry (Hold Block Button To Spam)",
-    Callback = function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BinxGodteli/Auto-parry-op/main/Op.lua"))()
-    getgenv().SpamSpeed = 25
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BinxGodteli/Auto-parry/main/Op-spam.lua"))()
-    local StarterGui = game:GetService("StarterGui")
+ local function chooseNewFocusedBall()
+    local balls = ballsFolder:GetChildren()
+    for _, ball in ipairs(balls) do
+        if ball:GetAttribute("realBall") ~= nil and ball:GetAttribute("realBall") == true then
+            focusedBall = ball
+            print(focusedBall.Name)
+            break
+        elseif ball:GetAttribute("target") ~= nil then
+            focusedBall = ball
+            print(focusedBall.Name)
+            break
+        end
+    end
+    
+    if focusedBall == nil then
+        print("Debug: Could not find a ball that's the realBall or has a target")
+        wait(1)
+        chooseNewFocusedBall()
+    end
+    return focusedBall
+end
+
+local function getDynamicThreshold(ballVelocityMagnitude)
+    if ballVelocityMagnitude > 60 then
+        return math.max(0.20, BASE_THRESHOLD - (ballVelocityMagnitude * VELOCITY_SCALING_FACTOR_FAST))
+    else
+        return math.min(0.01, BASE_THRESHOLD + (ballVelocityMagnitude * VELOCITY_SCALING_FACTOR_SLOW))
+    end
+end
+
+local function timeUntilImpact(ballVelocity, distanceToPlayer, playerVelocity)
+    if not character then return end
+    local directionToPlayer = (character.HumanoidRootPart.Position - focusedBall.Position).Unit
+    local velocityTowardsPlayer = ballVelocity:Dot(directionToPlayer) - playerVelocity:Dot(directionToPlayer)
+    
+    if velocityTowardsPlayer <= 0 then
+        return math.huge
+    end
+    
+    return (distanceToPlayer - sliderValue) / velocityTowardsPlayer
+end
+
+local function updateDistanceVisualizer()
+    local charPos = character and character.PrimaryPart and character.PrimaryPart.Position
+    if charPos and focusedBall then
+        if distanceVisualizer then
+            distanceVisualizer:Destroy()
+        end
+
+        local timeToImpactValue = timeUntilImpact(focusedBall.Velocity, (focusedBall.Position - charPos).Magnitude, character.PrimaryPart.Velocity)
+        local ballFuturePosition = focusedBall.Position + focusedBall.Velocity * timeToImpactValue
+
+        distanceVisualizer = Instance.new("Part")
+        distanceVisualizer.Size = Vector3.new(1, 1, 1)
+        distanceVisualizer.Anchored = true
+        distanceVisualizer.CanCollide = false
+        distanceVisualizer.Position = ballFuturePosition
+        distanceVisualizer.Parent = workspace    
+    end
+end
+
+local function checkIfTarget()
+    for _, v in pairs(ballsFolder:GetChildren()) do
+        if v:IsA("Part") and v.BrickColor == BrickColor.new("Really red") then 
+            print("Ball is targetting player | LDQ HUB")
+            return true 
+        end 
+    end 
+    return false
+end
+
+local function isCooldownInEffect(uigradient)
+    return uigradient.Offset.Y < 0.5
+end
+
+
+local function checkBallDistance()
+    if not character or not checkIfTarget() then return end
+
+    local charPos = character.PrimaryPart.Position
+    local charVel = character.PrimaryPart.Velocity
+
+    if focusedBall and not focusedBall.Parent then
+        print("Focused ball lost parent, choosing a new focused ball | LDQ HUB")
+        chooseNewFocusedBall()
+    end
+    if not focusedBall then 
+        print("No focused ball | LDQ HUB")
+        chooseNewFocusedBall()
+    end
+
+    local ball = focusedBall
+    local distanceToPlayer = (ball.Position - charPos).Magnitude
+    local ballVelocityTowardsPlayer = ball.Velocity:Dot((charPos - ball.Position).Unit)
+    
+    if distanceToPlayer < 15 then
+        parryButtonPress:Fire()
+        task.wait()
+    end
+
+    if timeUntilImpact(ball.Velocity, distanceToPlayer, charVel) < getDynamicThreshold(ballVelocityTowardsPlayer) then
+        if (character.Abilities["Raging Deflection"].Enabled or character.Abilities["Rapture"].Enabled) and UseRage == true then
+            if not isCooldownInEffect(uigrad2) then
+                abilityButtonPress:Fire()
+            end
+
+            if isCooldownInEffect(uigrad2) and not isCooldownInEffect(uigrad1) then
+                parryButtonPress:Fire()
+                if notifyparried == true then
+                    local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+                    local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+                    local StarterGui = game:GetService("StarterGui")
+                    StarterGui:SetCore("SendNotification", {
+                        Title = "LDQ HUB",
+                        Text = "Manually Parried Ball!",
+                        Duration = 1,
+                        Icon = "rbxassetid://14467433545"
+                    })
+                end
+            end
+
+        elseif not isCooldownInEffect(uigrad1) then
+            print(isCooldownInEffect(uigrad1))
+            parryButtonPress:Fire()
+            if notifyparried == true then
+                local StarterGui = game:GetService("StarterGui")
+                StarterGui:SetCore("SendNotification", {
+                    Title = "LDQ HUB",
+                    Text = "Automatically Parried Ball!",
+                    Duration = 1,
+                    Icon = "rbxassetid://14467433545"
+                })
+            end
+            task.wait(0.3)
+        end
+    end
+end
+
+
+local function autoParryCoroutine()
+    while isRunning do
+        checkBallDistance()
+        updateDistanceVisualizer()
+        task.wait()
+    end
+end
+
+
+
+localPlayer.CharacterAdded:Connect(function(newCharacter)
+    character = newCharacter
+    chooseNewFocusedBall()
+    updateDistanceVisualizer()
+end)
+
+localPlayer.CharacterRemoving:Connect(function()
+    if distanceVisualizer then
+        distanceVisualizer:Destroy()
+        distanceVisualizer = nil
+    end
+end)
+
+
+
+local function startAutoParry()
+    print("Started Auto Parry | LDQ HUB")
+    
+    chooseNewFocusedBall()
+    
+    isRunning = true
+    local co = coroutine.create(autoParryCoroutine)
+    coroutine.resume(co)
+end
+
+local function stopAutoParry()
+    isRunning = false
+end
+
+local parryon = false
+local autoparrydistance = 10
+ 
+ local Debug = false -- Set this to true if you want my debug output.
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local Player = Players.LocalPlayer or Players.PlayerAdded:Wait()
+local Remotes = ReplicatedStorage:WaitForChild("Remotes", 9e9) -- A second argument in waitforchild what could it mean?
+local Balls = workspace:WaitForChild("Balls", 9e9)
+
+-- Functions
+
+local function print(...) -- Debug print.
+    if Debug then
+        warn(...)
+    end
+end
+
+local function VerifyBall(Ball) -- Returns nil if the ball isn't a valid projectile; true if it's the right ball.
+    if typeof(Ball) == "Instance" and Ball:IsA("BasePart") and Ball:IsDescendantOf(Balls) and Ball:GetAttribute("realBall") == true then
+        return true
+    end
+end
+
+local function IsTarget() -- Returns true if we are the current target.
+    return (Player.Character and Player.Character:FindFirstChild("Highlight"))
+end
+
+local function Parry() -- Parries.
+    Remotes:WaitForChild("ParryButtonPress"):Fire()
+end
+-- The actual code
+
+Balls.ChildAdded:Connect(function(Ball)
+    if not VerifyBall(Ball) then
+        return
+    end
+    
+    print("Ball Spawned: {Ball}")
+    
+    local OldPosition = Ball.Position
+    local OldTick = tick()
+    
+    Ball:GetPropertyChangedSignal("Position"):Connect(function()
+        if IsTarget() then
+            local Distance = (Ball.Position - workspace.CurrentCamera.Focus.Position).Magnitude
+            local Velocity = (OldPosition - Ball.Position).Magnitude 
+            
+            print("Distance: {Distance}\nVelocity: {Velocity}\nTime: {Distance / Velocity}")
+        
+            if (Distance / Velocity) <= autoparrydistance then
+                if parryon == true then
+                    Parry()
+                end
+            end
+        end
+        
+        if (tick() - OldTick >= 1/60) then
+            OldTick = tick()
+            OldPosition = Ball.Position
+        end
+    end)
+end)
+ 
+ AutoParry:AddSection({
+ 	Name = "Bypass anticheat is active"
+ })
+ 
+AutoParry:AddToggle({
+	Name = "Auto Parry",
+    Callback = function(Value)
+    if Value then
+            parryon = Value
+            startAutoParry()
+            local StarterGui = game:GetService("StarterGui")
             StarterGui:SetCore("SendNotification", {
                 Title = "LDQ HUB",
                 Text = "Auto Parry has been started!",
                 Duration = 3,
-    })
-    end
-    })
-
-AutoParry:AddToggle({
-	Name = "Bypass Anticheat (Don't turn off)",
-	Default = true,
-    Callback = function()
-    local StarterGui = game:GetService("StarterGui")
+            })
+        else
+            stopAutoParry()
+            local StarterGui = game:GetService("StarterGui")
             StarterGui:SetCore("SendNotification", {
                 Title = "LDQ HUB",
-                Text = "Bypass Anticheat has been started!",
+                Text = "Auto Parry has been disabled!",
                 Duration = 3,
-    })
+            })
+        end
+    end,
+})
+
+AutoParry:AddSlider({
+    Name = "Distance Configuration",
+    Min = 5,
+    Max = 20,
+    Default = 11,
+    Color = Color3.fromRGB(255,255,255),
+    Increment = 0.5,
+    ValueName = "Distance",
+    Callback = function(Value)
+        autoparrydistance = Value
+    end,
+ })
+
+AutoParry:AddButton({
+	Name = "Spam (Hold Block Button To Spam)",
+    Callback = function()
+getgenv().SpamSpeed = 25
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BinxGodteli/Auto-parry/main/Op-spam.lua"))()
+end
+})
+
+AutoParry:AddToggle({
+    Name = "Auto Farm (need turn on auto parry)",
+    Callback = function(Value)
+        if Value then
+            local StarterGui = game:GetService("StarterGui")
+                    StarterGui:SetCore("SendNotification", {
+                        Title = "LDQ HUB",
+                        Text = "Auto Farm has been started!",
+                        Duration = 3,
+                    })
+            getgenv().god = true
+while getgenv().god and task.wait() do
+    for _,ball in next, workspace.Balls:GetChildren() do
+        if ball then
+            if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position, ball.Position)
+                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = ball.CFrame * CFrame.new(0, -10, (ball.Velocity).Magnitude * -0.5)
+            end
+        end
     end
-    })
+end
+        end
+        if not Value then
+            getgenv().god = false
+            local StarterGui = game:GetService("StarterGui")
+                    StarterGui:SetCore("SendNotification", {
+                        Title = "LDQ HUB",
+                        Text = "Auto Farm has been disabled!",
+                        Duration = 3,
+                    })
+        end
+    end,
+})
 
 local Ability = Window:MakeTab({
 	Name ="Abilities",

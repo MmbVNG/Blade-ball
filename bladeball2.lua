564c02f2231629f16fb0b82181ba1cfece67f8d4
MmbVNG <148338223+MmbVNG@users.noreply.github.com>
2023-11-07 12:43:18 +0700

    Update bladeball2.lua

--- bladeball2.lua
+++ bladeball2.lua
@@ -61,117 +61,64 @@ local autoparrydistance = 10
 
  
 
-local Debug = false -- Set this to true if you want my debug output.
-
+local UserInputService = game:GetService("UserInputService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Players = game:GetService("Players")
+local LocalPlayer = game:GetService("Players")
+local Balls = workspace:WaitForChild("Balls")
+local Player = LocalPlayer.LocalPlayer or LocalPlayer.PlayerAdded:Wait()
 
+local IsTargeted = false
+local CanHit = false
 
-local Player = Players.LocalPlayer or Players.PlayerAdded:Wait()
-
-local Remotes = ReplicatedStorage:WaitForChild("Remotes", 9e9) -- A second argument in waitforchild what could it mean?
-
-local Balls = workspace:WaitForChild("Balls", 9e9)
-
-
--- Functions
-
-
-local function print(...) -- Debug print.
-
-        if Debug then
-
-                warn(...)
-
+function VerifyBall1()
+local RealBall
+for i, v in pairs(Balls:GetChildren()) do
+        if v:GetAttribute("realBall") == true then
+  RealBall = v
         end
-
-end
-
-
-local function VerifyBall(Ball) -- Returns nil if the ball isn't a valid projectile; true if it's the right ball.
-
-        if typeof(Ball) == "Instance" and Ball:IsA("BasePart") and Ball:IsDescendantOf(Balls) and Ball:GetAttribute("realBall") == true then
-
-                return true
-
         end
+    return RealBall
+  end
+  
+function IsTarget1()
+    return (Player.Character and Player.Character:FindFirstChild("Highlight"))
 
 end
 
-
-local function IsTarget() -- Returns true if we are the current target.
-
-        return (Player.Character and Player.Character:FindFirstChild("Highlight"))
-
-end
-
-
-local function Parry() -- Parries.
-
-        Remotes:WaitForChild("ParryButtonPress"):Fire()
-
-end
-
-
--- The actual code
-
-
-Balls.ChildAdded:Connect(function(Ball)
-
-        if not VerifyBall(Ball) then
-
-                return
-
+function DetectBall()
+    local Ball = VerifyBall1()
+    
+  	if Ball then
+        local BallVelocity = Ball.Velocity.Magnitude
+        local BallPosition = Ball.Position
+  
+        local PlayerPosition = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+  
+        local Distance = (BallPosition - PlayerPosition).Magnitude
+        local PingAccountability = BallVelocity * (game.Stats.Network.ServerStatsItem["Data Ping"]:GetValue() / 1000)
+  
+        Distance -= PingAccountability
+        Distance -= 3.7
+  
+        local Hit = Distance / BallVelocity
+  
+        if Hit <= 0.5 then
+            return true
         end
-
-        
-
-        print(`Ball Spawned: {Ball}`)
-
-        
-
-        local OldPosition = Ball.Position
-
-        local OldTick = tick()
-
-        
-
-        Ball:GetPropertyChangedSignal("Position"):Connect(function()
-
-                if IsTarget() then -- No need to do the math if we're not being attacked.
-
-                        local Distance = (Ball.Position - workspace.CurrentCamera.Focus.Position).Magnitude
-
-                        local Velocity = (OldPosition - Ball.Position).Magnitude -- Fix for .Velocity not working. Yes I got the lowest possible grade in accuplacer math.
-
-                        
-
-                        print(`Distance: {Distance}\nVelocity: {Velocity}\nTime: {Distance / Velocity}`)
-
-                
-
-                        if (Distance / Velocity) <= autoparrydistance then -- Sorry for the magic number. This just works. No, you don't get a slider for this because it's 2am.
-if parryon == true then
-                                Parry()
-
-                        end
-
-                end
-
+    end
+    return false
 end
-                
 
-                if (tick() - OldTick >= 1/60) then -- Don't want it to update too quickly because my velocity implementation is aids. Yes, I tried Ball.Velocity. No, it didn't work.
+function DeflectBall()
+    if IsTargeted and DetectBall() then
+            ReplicatedStorage.Remotes.ParryButtonPress:Fire()
+end
+end
 
-                        OldTick = tick()
-
-                        OldPosition = Ball.Position
-
-                end
-
-        end)
-
+game:GetService('RunService').PostSimulation:Connect(function()
+    IsTargeted = IsTarget1()
+        DeflectBall()
 end)
 
 local Window = Fluent:CreateWindow({
@@ -198,7 +145,7 @@ local Options = Fluent.Options
 
     AutoParryToggle:OnChanged(function(Value)
         if Value then
-            parryon = true
+            parryon1 = true
             local StarterGui = game:GetService("StarterGui")
             StarterGui:SetCore("SendNotification", {
                 Title = "Asmodeus Hub",
@@ -206,7 +153,7 @@ local Options = Fluent.Options
                 Duration = 3,
             })
         else
-            parryon = false
+            parryon1 = false
             local StarterGui = game:GetService("StarterGui")
             StarterGui:SetCore("SendNotification", {
                 Title = "Asmodeus Hub",
